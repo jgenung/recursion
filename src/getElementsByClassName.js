4f5bf8f78987e704760c31b8a8a2b15a50176c6e
Jordan Genung <jordanmgenung@gmail.com>
2015-01-19 16:43:04 -0600

    Recursion practice completed.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className)
+{
+	var node = document.body;
+	var array = [];
+	
+	var getElementsByName = function(node)
+	{
+		var classes = node.classList;
+		var children = node.childNodes;
+
+		if(classes  && (classes.length > 0) && classes.contains(className))
+		{
+				array.push(node);
+		}
+
+		if(children.length > 0)
+		{
+			for(var i = 0; i < children.length; i++)
+			{
+				getElementsByName(children[i]);
+			}
+		}
+	}
+	
+	getElementsByName(node);
+	return array;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj) 
+{
+  // Switch on object to test type
+  switch(testType(obj))
+  {
+    case "null": 
+      return "null";	
+    case "undefined":
+      return "";
+    case "number":
+      return obj.toString();
+    case "boolean":
+      return "" + obj;
+    case "string":
+      return "\"" + obj + "\"";
+    case "object":
+      if(_.contains(obj, "") || _.contains(obj, undefined)){obj = "";}
+      var objectVals = _.map(obj, function(val, key)
+      {
+        return stringifyJSON(key) + ":" + stringifyJSON(val); 
+      }).join(",");
+      return "{" + objectVals + "}";
+    case "array":
+			var content = _.map(obj, function(val){ 
+        return stringifyJSON(val); 
+      }).join(",");
+      return "[" + content + "]";
+  }
 };
+
+// Test item to see what type it is
+var testType = function(item)
+{
+	var type = typeof item;
+	if(item === null){ return "null"; }
+	if(Array.isArray(item)){ return "array"; }
+	return type;
+}
